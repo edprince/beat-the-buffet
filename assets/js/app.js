131973207848cb987ac86a2966abd616cf0a14ed
ed <edward_prince@hotmail.com>
2015-06-06 15:10:17 +0100

    function error

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,20 @@
+
+
+var bool = true;
+var count = 2;
+
 function update() {
-  var bool = true;
-  var count = 2;
-  while (bool === true && count < 100) {
-    console.log(count);
-    count += 1;
-    console.log(count);
-    console.log(count.toString() + "px");
-    //var query = '\"' + count.toString() + '%\"';
-    var query = count + '%';
-    console.log(query);
-    document.getElementById('howard').style.value = query;
-  }
+  console.log(count);
+  count += 1;
+  console.log(count);
+  console.log(count.toString() + "px");
+  //var query = '\"' + count.toString() + '%\"';
+  var query = count + '%';
+  console.log(query);
+  document.getElementById('howard').style.value = query;
 }
 
 function playGame() {
-  setInterval(update(), 1000);
+  setInterval(update, 1000);
 }
 
